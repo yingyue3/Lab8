e07f7c8a616256afd089520f53805e2d857692bb
yingyue3 <yingyue3@ualberta.ca>
2022-10-31 14:58:32 -0600

    add deleteCity correctly

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -68,7 +68,11 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void deleteCity(City city) {
-
+        if (cities.contains(city)){
+            cities.remove(city);
+        }else{
+            throw new IllegalArgumentException();
+        }
     }
 
 }
